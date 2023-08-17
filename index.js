38d37ab63c097a8f820f5e8348b10293f1e5e3f8
Steve Hommy <steve96hommy@gmail.com>
2023-08-17 16:04:15 +0300

    Part 3.20 done

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const Person = require('./models/person')
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static('build'));
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
 
 app.use((request, response, next) => {
     morgan.token('body', (request) => JSON.stringify(request.body));
@@ -20,6 +20,10 @@ app.use((request, response, next) => {
         morgan(':method :url :status :res[content-length] - :response-time ms')(request, response, next);
     }
 });
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
@@ -76,8 +80,8 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({ error: 'Content missing' })
+    if (!body.name) {
+        return response.status(400).json({ error: 'Name missing' })
     }
 
     const person = new Person({
@@ -85,27 +89,35 @@ app.post('/api/persons', (request, response, next) => {
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
         .catch(error => next(error))
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
     const { name, number } = request.body
 
+    /*
     const person = {
         name: body.name,
         number: body.number
     }
+    */
 
-    Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
+    Person.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
         .then(updatePerson => {
             response.json(updatePerson)
         })
         .catch(error => next(error))
 })
 
+app.use(unknownEndpoint)
 app.use(errorHandler)
 
 const PORT = process.env.PORT
