3e26c7091d5b1587e7f7d07d1a3364d56c7e9b2f
Steve Hommy <steve96hommy@gmail.com>
2023-08-17 12:08:17 +0300

    Part 3.19 done

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.static('build'));
 
 app.use((request, response, next) => {
     morgan.token('body', (request) => JSON.stringify(request.body));
+
     if (request.method === 'POST') {
         morgan(':method :url :status :res[content-length] - :response-time ms :body')(request, response, next);
     } else {
@@ -22,8 +23,11 @@ app.use((request, response, next) => {
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
+
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'Malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
@@ -69,14 +73,13 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'Content missing'
-        })
+        return response.status(400).json({ error: 'Content missing' })
     }
+
     const person = new Person({
         name: body.name,
         number: body.number
@@ -85,17 +88,18 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+        .catch(error => next(error))
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
 
     const person = {
         name: body.name,
         number: body.number
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
         .then(updatePerson => {
             response.json(updatePerson)
         })
